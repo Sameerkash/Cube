338444d76299726337cf3ce00717b9b923052d42
“piyu99” <gitPersonal>
2023-10-14 13:38:19 +0530

    .gitignore is now working

--- lib/auth/secrets.dart
+++
@@ -1,4 +0,0 @@
-class Secrets {
-  static String squareAPIKey = 'Bearer EAAAEE0Vl4dTGw0exoIwB7Ttq92t50QkdGXkFE_r80rcfmMJIHEkJajpwWHLMTkj';
-  static String palmAPIKey = 'AIzaSyCffiqR_FEAoAmcSx7AgP_FnPt-jDpGyV0';
-}
